3710c042761b1f382429c8e9d0bf9ea978282f24
Tolga Pirim <tolgapirim25@gmail.com>
2023-07-28 18:10:14 +0300

    Added bottom_nav_menu change theme to MaterialComponents from Material3

--- app/src/main/java/com/prmto/inviostaj/presentation/MainActivity.kt
+++ app/src/main/java/com/prmto/inviostaj/presentation/MainActivity.kt
@@ -3,6 +3,9 @@ package com.prmto.inviostaj.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.prmto.inviostaj.R
 import com.prmto.inviostaj.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,13 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        val navController = navHost.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
