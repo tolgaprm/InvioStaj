d9d5d97df1c5c881f1777158edbf74e216382f9d
Tolga Pirim <tolgapirim25@gmail.com>
2023-07-28 17:35:17 +0300

    setup project

---
+++ app/build.gradle.kts
@@ -0,0 +1,89 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
+plugins {
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs")
+}
+
+android {
+    namespace = "com.prmto.inviostaj"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.prmto.inviostaj"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    //dagger - hilt
+    implementation(libs.dagger.hilt)
+    kapt(libs.dagger.hilt.compiler)
+    kapt(libs.hilt.compiler)
+
+    // navigation
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
+
+    // coroutines for getting off the UI thread
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+
+    // Room
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
+    kapt(libs.room.compiler)
+    implementation(libs.room.ktx)
+
+    // retrofit for networking
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.coroutines.adapter)
+    implementation(libs.retrofit.converter.moshi)
+
+    // moshi for parsing the JSON format
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    kapt(libs.moshi.kotlin.codegen)
+
+    implementation(libs.splashscreen)
+
+    // Coil
+    implementation(libs.coil)
+
+    //Paging Library
+    implementation(libs.paging.runtime.ktx)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath(libs.hilt.android.gradle.plugin)
+
+    }
+}
+plugins {
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.navigation.safe.args) apply false
+}
