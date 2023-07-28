07135f0c374d1b38925b33cf9f2f7eaee9f7948e
Tolga Pirim <tolgapirim25@gmail.com>
2023-07-28 17:46:34 +0300

    implemented splash screen

--- app/src/main/java/com/prmto/inviostaj/MainActivity.kt
+++ app/src/main/java/com/prmto/inviostaj/MainActivity.kt
@@ -2,10 +2,15 @@ package com.prmto.inviostaj
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.prmto.inviostaj.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
