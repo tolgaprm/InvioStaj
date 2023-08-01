7da986d4a0a4e42b36dc19be2f6baa8ef89a0f3b
Tolga Pirim <tolgapirim25@gmail.com>
2023-08-01 13:53:20 +0300

    + Add MovieEntity and MovieDao

---
+++ app/src/main/java/com/prmto/inviostaj/data/local/dao/MovieDao.kt
@@ -0,0 +1,20 @@
+package com.prmto.inviostaj.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.prmto.inviostaj.data.local.entity.MovieEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MovieDao {
+    @Query("SELECT * FROM favorite_movie")
+    fun getAllFavoriteMovies(): Flow<MovieEntity>
+
+    @Insert
+    suspend fun insertFavoriteMovie(movieEntity: MovieEntity)
+
+    @Delete
+    suspend fun deleteFavoriteMovie(movieEntity: MovieEntity)
+}
--- app/src/main/java/com/prmto/inviostaj/util/Constants.kt
+++ app/src/main/java/com/prmto/inviostaj/util/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
 
     const val PAGE_SIZE = 20
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
+    const val FAVORITE_MOVIE_ENTITY_TABLE_NAME = "favorite_movie"
 }
